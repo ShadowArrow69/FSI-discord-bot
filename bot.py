f9057e059939911cfa0d1c1e67f7233696332ffd
ShadowArrow69 <40230679+ShadowArrow69@users.noreply.github.com>
2018-06-15 18:53:57 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,4 +83,4 @@ async def on_message(message):
         with open(self.configPath, "r") as config:
             self.cfg = yaml.load(config)
 
-bot.run("token") 
+bot.run("NDU2MDQ0NTUwMTM4NDk1MDA3.DgUO3Q.Qh0qBu4iHZG_ZNWfpN63uCCvuD4") 
