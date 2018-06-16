89e8dd5efceb66fef051eb6635f386a72e8b9ece
ShadowArrow69 <40230679+ShadowArrow69@users.noreply.github.com>
2018-06-16 20:39:53 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,19 +16,7 @@ async def echo(*, message: str):
 @bot.command()
 async def add(ctx, a: int, b: int):
     await ctx.send(a+b)
-
-@bot.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
-
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hi bitch!")
-
-@bot.cmmands()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
-    
+ 
 @bot.event 
 async def on_ready():
     print("Bot is online and connected to Discord") 
