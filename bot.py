177ff7d080755aaeaa76a97bdbf4686dd61b3cf4
ShadowArrow69 <40230679+ShadowArrow69@users.noreply.github.com>
2018-06-16 19:51:51 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,4 +73,4 @@ async def on_message(message):
         await bot.send_message(message.channel, "Status has been changed to 'playing" + game + "'")
 
         
-bot.run("NDU3MzEzMDUwODQ5MDUwNjM3.DgXRgg.D-pjEkgc0Mx4Q4orZsquQ3CTBfQ") 
+bot.run("NDU3MzEzMDUwODQ5MDUwNjM3.DgZvJA.0a4dhcwwD8STr0KT27r3PrIMiCc") 
