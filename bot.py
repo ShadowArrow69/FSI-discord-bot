097a3c132841219f7bd3a70e8d614f69d7e5ccfb
ShadowArrow69 <40230679+ShadowArrow69@users.noreply.github.com>
2018-06-16 20:54:55 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,6 @@ chat_filter = ["NIGGA", "HENRY", "FUCK"]
 bypass_list = ["253417332406222848"]
 
 @bot.command()
-async def echo(*, message: str):
-    await bot.say(message)
-
-@bot.command()
 async def add(ctx, a: int, b: int):
     await ctx.send(a + b)
  
