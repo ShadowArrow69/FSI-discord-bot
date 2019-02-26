7deae4f3c24766ba8e7babe3c174d14cd7cd39ee
ShadowArrow69 <40230679+ShadowArrow69@users.noreply.github.com>
2019-02-26 21:20:08 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,6 +73,8 @@ async def on_message(message):
         await bot.send_message(message.channel, ":hear_no_evil:")
     if message.content == "nicolas":
         await bot.send_message(message.channel, ":pig:")
+    if message.content == "william":
+        await bot.send_message(message.channel, ":baby::skin-tone-5:")     
     if message.content == "yummy":
         await bot.send_message(message.channel, ":hamburger: :fries: :taco: :ramen: :stew: :bento: :ice_cream: :chocolate_bar: :cookie:")
     if message.content == "allahu akbar":
